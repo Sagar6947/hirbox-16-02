ddb38fa7891d88f1fe0efef3de76f3ad42cd1199
Muskan <developerjain19@gmail.com>
2023-02-17 13:18:58 +0530

    last

--- application/views/company/dashboard.php
+++ application/views/company/dashboard.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 
+
+
 <head>
 
     <meta charset="utf-8" />
@@ -9,7 +11,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
-    <!-- App favicon -->
+    <!-- App favicon sagar -->
+
 
     <?php include 'includes/headerlink.php'; ?>
 
